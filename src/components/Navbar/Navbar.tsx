b71f82501974b98b1ff92e40e6b5f40d31dac1b9
Shajahan A <Shajansheriff@users.noreply.github.com>
2023-02-19 10:28:21 +0530

    use switch in navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,14 @@
+import { Switch } from "../Switch";
 import styles from "./Navbar.module.css";
+
 export function Navbar() {
   return (
     <header className={styles.navbar}>
       <div className={styles.brand}>Brand</div>
       <ul>
-        <li>menu 1</li>
+        <li>
+          <Switch />
+        </li>
       </ul>
     </header>
   );
