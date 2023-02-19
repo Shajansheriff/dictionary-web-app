12398f4192fdc2da553615688e0a40b275d57305
Shajahan A <Shajansheriff@users.noreply.github.com>
2023-02-19 11:14:51 +0530

    update page with content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,53 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main>
-      <Divider>noun</Divider>
       <InputSearch />
+
+      <header>
+        <div>
+          <h1>keyboard</h1>
+          <h2>/ˈkiːbɔːd/</h2>
+        </div>
+      </header>
+      <Divider>noun</Divider>
+      <section>
+        <h3>Meaning</h3>
+        <ul>
+          <li>
+            (etc.) A set of keys used to operate a typewriter, computer etc.
+          </li>
+          <li>
+            A component of many instruments including the piano, organ, and
+            harpsichord consisting of usually black and white keys that cause
+            different tones to be produced when struck.
+          </li>
+        </ul>
+        <div>
+          <h3>Synonyms</h3>
+          <a href="#">electronic keyboard</a>
+          <a href="#">electronic keyboard</a>
+          <a href="#">electronic keyboard</a>
+          <a href="#">electronic keyboard</a>
+        </div>
+      </section>
+      <Divider>verb</Divider>
+      <section>
+        <h3>Meaning</h3>
+        <ul>
+          <li>
+            (etc.) A set of keys used to operate a typewriter, computer etc.
+          </li>
+          <li>
+            A component of many instruments including the piano, organ, and
+            harpsichord consisting of usually black and white keys that cause
+            different tones to be produced when struck.
+          </li>
+        </ul>
+        <div>
+          <h3>Synonyms</h3>
+          <a href="#">electronic keyboard</a>
+        </div>
+      </section>
     </main>
   );
 }
